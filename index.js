7153d571ba58edc6577ef5cccc451ca497992487
SmallZhiLack <120072472+SmallZhiLack@users.noreply.github.com>
2022-12-08 15:30:10 +0800

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify("Hello from Github and is updated")
-    }
-    return response
-}
