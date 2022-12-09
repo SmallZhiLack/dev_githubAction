7fed899a7e9b60071af331af8723a4623bc1d5da
ZhiLin <rd19@westar.tw>
2022-12-09 11:02:14 +0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify("Hello from Github and is updated test_js")
+        body: JSON.stringify("Hello from Github and is updated test_js again up")
     }
     return response
 }
